c6583b4f4856ad04f85ed4e11686a8a297f9a0cb
AtifAhmed29 <63032286+AtifAhmed29@users.noreply.github.com>
2022-03-13 15:48:17 +0600

    commit 1

---
+++ callback.js
@@ -0,0 +1,14 @@
+
+//arrow function
+var isEven=(ele)=>{
+    return ele%2===0;
+}
+
+console.log(isEven(2));
+
+//call back function
+var result=[2,8,4,4,6].every((e)=> e%2===0);
+
+// when you do not return anythis you must not use {} in call bcak fn
+
+console.log(result);
